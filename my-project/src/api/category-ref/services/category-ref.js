ac344954b395a6e02b6600e73a49a4ca1a023949
luka1111 <78353355+exco-consulting@users.noreply.github.com>
2023-06-14 11:20:05 +0000

    Update 10 files

---
+++ my-project/src/api/category-ref/services/category-ref.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * category-ref service
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::category-ref.category-ref');
